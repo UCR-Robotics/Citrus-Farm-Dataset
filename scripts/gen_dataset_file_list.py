f5bb77a35b79843ce52131e0fae479c7b3c2c549
Hanzhe Teng <hteng007@ucr.edu>
2023-10-29 20:07:37 -0700

    update file list with md5

--- scripts/gen_dataset_file_list.py
+++ scripts/gen_dataset_file_list.py
@@ -1,23 +1,45 @@
 import os
+import hashlib
 import yaml
+from tqdm import tqdm
+
+def ComputeMD5(file_path):
+  hash_md5 = hashlib.md5()
+  with open(file_path, "rb") as f:
+    for chunk in iter(lambda: f.read(4096), b""):
+      hash_md5.update(chunk)
+  return hash_md5.hexdigest()
 
 def GenerateYAMLConfig(root_dir):
-  config_dict = {'folders': {}}
+  config_dict = {'citrus-farm-dataset': {}}
+
+  # Count total number of files for the progress bar
+  total_files = 0
+  for _, _, filenames in os.walk(root_dir):
+    total_files += len(filenames)
+
+  pbar = tqdm(total=total_files, desc="Processing files", dynamic_ncols=True)
 
   # Traverse the directory tree rooted at root_dir
   for dirpath, dirnames, filenames in os.walk(root_dir):
     # Skip the root directory itself
     if dirpath == root_dir:
       continue
-    
+
     # Extract the relative directory name
     rel_dir = os.path.relpath(dirpath, root_dir)
 
-    # Sort filenames alphanumerically
-    sorted_filenames = sorted(filenames)
-    
+    # Sort filenames alphanumerically and compute MD5 checksums
+    file_info = {}
+    for file_name in sorted(filenames):
+      full_file_path = os.path.join(dirpath, file_name)
+      file_info[file_name] = {"md5": ComputeMD5(full_file_path)}
+      pbar.update(1)  # Update the progress bar
+
     # Update the dictionary
-    config_dict['folders'][rel_dir] = sorted_filenames
+    config_dict['citrus-farm-dataset'][rel_dir] = file_info
+
+  pbar.close()
 
   # Serialize to YAML
   with open("dataset_file_list.yaml", "w") as yaml_file:
@@ -26,4 +48,3 @@ def GenerateYAMLConfig(root_dir):
 if __name__ == "__main__":
   root_dir = "."  # Replace with the path to your local root directory
   GenerateYAMLConfig(root_dir)
-
